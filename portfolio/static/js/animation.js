bf546d54c898f0ffd9474fcb5c2c5802145b212a
Kossatz <atyllakossatz@gmail.com>
2024-11-29 12:57:17 +0000

    feat: integrate Tailwind CSS and update various sections
    - Installed Tailwind CSS library for styling
    - Updated carousel slide design and behavior
    - Enhanced animation.js for smoother interactions
    - Linked contact section to navigation menu
    - Updated project section layout and content

--- portfolio/static/js/animation.js
+++ portfolio/static/js/animation.js
@@ -1,5 +1,4 @@
 
-
 // Suaviza navegação entre seções
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener("click", function (e) {
@@ -9,4 +8,19 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
             behavior: "smooth",
         });
     });
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+effect: "cube",
+grabCursor: true,
+cubeEffect: {
+    shadow: true,
+    slideShadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.94,
+},
+pagination: {
+    el: ".swiper-pagination",
+},
 });
